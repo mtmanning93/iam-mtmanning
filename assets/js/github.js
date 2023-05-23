53cf85c4a1de19beda6cd405a14be2747fbe4d18
mtmanning93 <>
2023-05-23 05:21:25 +0000

    Add button, modal and style for github api

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,25 +1,23 @@
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
-            <span class="small-name">
+            <span id="gh-username-link">
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
-            <div class="gh-avatar">
+            <div>
                 <a href="${user.html_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
-
 }
 
 function repoInformationHTML(repos) {
     if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;
+        return `<div class="repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function (repo) {
@@ -28,55 +26,54 @@ function repoInformationHTML(repos) {
                 </li>`;
     });
 
-    return `<div class="clearfix repo-list">
+    return `<div class="repo-list">
                 <p>
-                    <strong>Repo list:</strong>
+                    <strong>Repo List:</strong>
                 </p>
                 <ul>
-                    ${listItemsHTML.jon("/\n")}
+                    ${listItemsHTML.join("\n")}
                 </ul>
             </div>`;
 }
 
 function fetchGitHubInformation(event) {
-    $("github-user-data").html("");
-    $("github-repo-data").html("");
+    $("#github-user-data").html("");
+    $("#github-repo-data").html("");
 
     var username = $("#github-username").val();
     if (!username) {
-        $("github-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        $("#github-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
-    $("#github-user-data").html(`
-        <div id="loader">
-            <img src="assets/css/loader100px.gif" alt="loading..." /> 
+    $("#github-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
-        $.getJSON(`https://api.github.com/users/${username}/repos`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function (firstResponse, secondResponse) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#github-user-data").html(userInformationHTML(userData));
             $("#github-repo-data").html(repoInformationHTML(repoData));
-        }, function (errorResponse) {
+        },
+        function (errorResponse) {
             if (errorResponse.status === 404) {
-                $("##github-user-data").html(
-                    `<h2>No information found for user: ${username}</h2>`
-                );
+                $("#github-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else if (errorResponse.status === 403) {
                 var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
-                $("github-user-data").html(`<h4>Too many API requests, please wait until ${restTime.toLocaleTimeString()}</h4>`);
+                $("#github-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#github-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
-                );
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        }
-    );
+        });
 }
+
 $(document).ready(fetchGitHubInformation);
